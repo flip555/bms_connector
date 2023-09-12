370c425e6bf770d96f108158bb04893b75c8abd0
flip555 <109870462+flip555@users.noreply.github.com>
2023-09-12 01:32:35 +0100

    update config flow

--- custom_components/seplos_bms_ha/config_flow.py
+++ custom_components/seplos_bms_ha/config_flow.py
@@ -1,4 +1,3 @@
-"""Adds config flow for Seplos BMS."""
 from __future__ import annotations
 
 import voluptuous as vol
@@ -10,14 +9,17 @@ class SeplosBMSFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     def is_valid_port(self, port: str) -> bool:
         """Validate if the provided port exists and has permissions."""
-        # Validate if it's a valid /dev/ path or the longer /dev/serial/... path
+        # Validate if it's a valid /dev/ path or the longer /dev/serial/by-id/ path
         if port.startswith("/dev/ttyUSB") or port.startswith("/dev/serial/by-id/"):
             return True
         return False
-        
+
     async def async_step_user(self, user_input: dict | None = None) -> config_entries.FlowResult:
         """Handle a flow initialized by the user."""
         _errors = {}
+
+        # Predefined set of USB ports for the dropdown
+        predefined_ports = ["/dev/ttyUSB0", "/dev/ttyUSB1", "/dev/ttyUSB2", "/dev/ttyUSB3"]
 
         if user_input is not None:
             # If RS485-USB is selected, validate the USB port
@@ -27,11 +29,13 @@ class SeplosBMSFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 else:
                     return self.async_create_entry(title="Seplos BMS", data=user_input)
 
+        data_schema = vol.Schema({
+            vol.Required("source"): vol.In(["RS485-USB"]),
+            vol.Optional("usb_port", default=predefined_ports[0] if predefined_ports else ""): str,
+        })
+
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("source"): vol.In(["RS485-USB"]),
-                vol.Optional("usb_port", default=""): str,  
-            }),
+            data_schema=data_schema,
             errors=_errors,
         )
