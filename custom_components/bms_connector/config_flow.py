c0e3ab2f3642e352113c636f8dbe5ae081ed716b
flip555 <109870462+flip555@users.noreply.github.com>
2023-09-15 22:06:36 +0100

    hacs json config flow with languages

--- custom_components/bms_connector/config_flow.py
+++ custom_components/bms_connector/config_flow.py
@@ -1,35 +1,68 @@
-import voluptuous as vol
 from homeassistant import config_entries
+import voluptuous as vol
+
 from .const import DOMAIN, BMS_TYPES
 
 class BMSConnectorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    def is_valid_port(self, port: str) -> bool:
-        """Validate if the provided port exists and has permissions."""
-        try:
-            with serial.Serial(port, baudrate=9600, timeout=1):
-                return True
-        except (OSError, serial.SerialException):
-            return False
-
     async def async_step_user(self, user_input=None):
         errors = {}
 
-        if user_input is not None:
-            # Validate user input here
-            title = f"{user_input['bms_type']} - {user_input['battery_address']}"
-            return self.async_create_entry(title=title, data=user_input)
+        if user_input is not None and user_input.get("confirm"):
+            # If the user checks the box, proceed to the next step
+            return await self.async_step_bms_type()
 
         data_schema = vol.Schema({
-            vol.Required("bms_type", description="BMS Type"): vol.In(BMS_TYPES),
-            vol.Required("connector_port", description="Connector Port (e.g., /dev/ttyUSB0 or esp@ip:port)", default="/dev/ttyUSB0"): str,
-            vol.Required("battery_address", description="Battery Address [e.g., 0x00]", default="0x00"): str,
-            vol.Required("sensor_prefix", description="Sensor Prefix [e.g., Seplos BMS 0x00 -]", default="Seplos BMS V2 0x00 - "): str,
+            vol.Required("confirm", description="I confirm"): bool,
         })
-
         return self.async_show_form(
             step_id="user",
             data_schema=data_schema,
             errors=errors,
+        )
+
+    async def async_step_bms_type(self, user_input=None):
+        if user_input is not None:
+            # Store user input and transition to the next step
+            self.user_input = user_input
+            return await self.async_step_connector_port()
+
+        data_schema = vol.Schema({
+            vol.Required("bms_type", description="Please select your BMS"): vol.In(BMS_TYPES),
+        })
+
+        return self.async_show_form(
+            step_id="bms_type",
+            data_schema=data_schema,
+        )
+
+    async def async_step_connector_port(self, user_input=None):
+        if user_input is not None:
+            # Store user input and transition to the next step
+            self.user_input["connector_port"] = user_input["connector_port"]
+            return await self.async_step_sensor_prefix()
+
+        data_schema = vol.Schema({
+            vol.Required("connector_port", description="Please enter your port", default="/dev/ttyUSB0"): str,
+        })
+
+        return self.async_show_form(
+            step_id="connector_port",
+            data_schema=data_schema,
+        )
+
+    async def async_step_sensor_prefix(self, user_input=None):
+        if user_input is not None:
+            # Store user input and transition to the additional config step
+            title = f"{self.user_input['bms_type']} - {self.user_input['sensor_prefix']}"
+            return self.async_create_entry(title=title, data=self.user_input)
+
+        data_schema = vol.Schema({
+            vol.Required("sensor_prefix", description="Please enter your desired sensor prefix", default="Seplos BMS V2 0x00 - "): str,
+        })
+
+        return self.async_show_form(
+            step_id="sensor_prefix",
+            data_schema=data_schema,
         )
