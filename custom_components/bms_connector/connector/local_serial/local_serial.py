8cd18cc15fcac120d0a80b057ef3c5a78c5b807d
flip555 <109870462+flip555@users.noreply.github.com>
2023-09-16 06:50:57 +0100

    test code for esp and local serial loops

--- custom_components/bms_connector/connector/local_serial/local_serial.py
+++ custom_components/bms_connector/connector/local_serial/local_serial.py
@@ -1,12 +1,29 @@
 import serial
-import time
 import logging
-import serial
+
 _LOGGER = logging.getLogger(__name__)
 
-def send_serial_command(command: str, port: str, baudrate: int = 19200, timeout: int = 2) -> str:
+def send_serial_commands(commands, port, baudrate=19200, timeout=2):
+    responses = []
+
     with serial.Serial(port, baudrate=baudrate, timeout=timeout) as ser:
-        ser.write(command.encode())
-        time.sleep(0.5)
-        response = ser.read(ser.in_waiting).decode().replace('\r', '').replace('\n', '')
-        return response
+        for command in commands:
+            ser.write(command.encode())
+            response = ''
+            while True:
+                char = ser.read(1).decode()
+                if char == '\n' or ser.timeout:
+                    break
+                response += char
+            _LOGGER.debug("Response:")
+            _LOGGER.debug(response)
+            responses.append(response)
+
+    return responses
+
+# Usage example:
+# commands = ["command1", "command2", "command3"]
+# port = "/dev/ttyUSB0"  # Replace with your serial port
+# responses = send_serial_commands(commands, port, baudrate=19200, timeout=2)
+# for i, response in enumerate(responses):
+#     _LOGGER.debug(f"Response {i + 1}: {response}")
