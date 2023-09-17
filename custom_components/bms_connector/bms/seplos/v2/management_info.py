59513c8518f8102febe8faa10941f1985da82f86
flip555 <109870462+flip555@users.noreply.github.com>
2023-09-17 14:59:59 +0100

    update main code to reflect 51H data correctly
    starting point seems to move when extracting through HA addon

--- custom_components/bms_connector/bms/seplos/v2/management_info.py
+++ custom_components/bms_connector/bms/seplos/v2/management_info.py
@@ -11,37 +11,23 @@ class BMSSystemDetails:
             f"software_version: {self.software_version}, "
             f"manufacturer_name: {self.manufacturer_name} "
         )
-        
-def hex_to_ascii(hex_str):
-    """Converts a hexadecimal string to an ASCII string."""
-    hex_values = hex_str.split()
-    ascii_str = ''.join(chr(int(hex_value, 16)) for hex_value in hex_values)
-    return ascii_str
 
-def decode_fiveone(message_bytes):
+def decode_fiveone(hex_string):
     result = BMSSystemDetails()
+    hex_string = bytes.fromhex(hex_string)
+    if len(hex_string) < 10:
+        return None
+    device_name_bytes = hex_string[0:10]
+    software_version_bytes = hex_string[10:12]
+    manufacturer_name_bytes = hex_string[12:24]
 
-    message_hex_str = message_bytes
+    # Convert bytes to ASCII strings
+    result.device_name = device_name_bytes.decode('ascii')
+    
+    # Interpret the software version correctly
+    software_version = int.from_bytes(software_version_bytes, byteorder='big') / 1000 * 4  # Really unsure if this is correct ... my version is 16.4 so * 4 made 4.1 16.4
+    result.software_version = "{:.1f}".format(software_version)
 
-    SOI = message_hex_str[0:2]
-    VER = message_hex_str[2:4]
-    ADR = message_hex_str[4:6]
-    CID1 = message_hex_str[6:8]
-    CID2 = message_hex_str[8:10]
-    LENGTH = message_hex_str[10:14]  # Fix the LENGTH slice
-    _LOGGER.debug("ATru. ADR: %s", ADR)
-
-    # Attempt to convert LENGTH to decimal
-    length_in_dec = int(LENGTH, 16) & 0x0FFF
-
-    INFO = message_hex_str[14:14 + length_in_dec * 2]
-    CHKSUM = message_hex_str[-4:]
-    _LOGGER.debug("INFO Field (hex): %s", ' '.join(hex(ord(char)) for char in INFO))
-
-    # Getting data from INFO
-    result.device_name = hex_to_ascii(INFO[16:36])  # Adjust slice for Device Name
-    result.software_version = int(float(INFO[0:4]), 16)  # Convert Software Version to integer
-    result.manufacturer_name = hex_to_ascii(INFO[36:])
-    _LOGGER.debug("Decoded values: %s", result)
+    result.manufacturer_name = manufacturer_name_bytes.decode('ascii')
 
     return result
