52d5e241c2418461c169a741b3168950b568114f
flip555 <109870462+flip555@users.noreply.github.com>
2024-02-24 22:56:03 +0000

    update

--- custom_components/bms_connector/config_flows/bms/seplos.py
+++ custom_components/bms_connector/config_flows/bms/seplos.py
@@ -3,25 +3,6 @@ import voluptuous as vol
 from ...const import DOMAIN
 
 class SeplosConfigFlowMethods:
-    async def async_step_seplos_bms_v2(self, user_input=None):
-        if user_input is not None:
-            # Store user input and create the configuration entry
-            self.user_input.update(user_input)
-            self.user_input["name_prefix"] = f"Seplos BMS V2 - {self.user_input['usb_port']} - "
-            self.user_input["sensor_update_frequency"] = 5
-            title = f"Seplos BMS V2 - {self.user_input['usb_port']}"
-            return self.async_create_entry(title=title, data=self.user_input)
-
-        data_schema = vol.Schema({
-                vol.Required("usb_port", default="/dev/ttyUSB0"): str,
-                vol.Required("battery_pack_count", default=1): vol.All(vol.Coerce(int), vol.Range(min=1, max=16)),
-        })
-
-        return self.async_show_form(
-            step_id="seplos_bms_v2",
-            data_schema=data_schema,
-        )
-
     async def async_step_seplos_bms_v2_device(self, user_input=None):
         if user_input is not None:
             # Store user input and create the configuration entry
