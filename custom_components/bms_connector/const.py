30815610a8d22d1446659359607a9a334c5ddcbb
flip555 <109870462+flip555@users.noreply.github.com>
2026-05-29 18:02:15 +0000

    Bump version to 1.4.0

--- custom_components/bms_connector/const.py
+++ custom_components/bms_connector/const.py
@@ -1,7 +1,7 @@
 # const.py
 NAME = "BMS Connector"
 DOMAIN = "bms_connector"
-VERSION = "1.3.4"
+VERSION = "1.4.0"
 ATTRIBUTION = "Integration for BMS via serial communication."
 
 CONF_CONNECTION_TYPE = "connection_type"
